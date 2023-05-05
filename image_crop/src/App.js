be50597f33df3828077ae022aae5ba3a7dfcee02
Shoydon <shoydon@gmail.com>
2023-05-05 19:50:40 +0530

    tried react-easy-crop and react-crop-image

--- image_crop/src/App.js
+++ image_crop/src/App.js
@@ -1,37 +1,39 @@
 import React from 'react';
-import ReactCrop from 'react-image-crop'
-import { useState, useRef } from "react";
+// import ReactCrop from 'react-image-crop'
+// import 'react-image-crop/dist/ReactCrop.css'
+// import Cropper from 'react-easy-crop'
+import Avatar from 'react-avatar-edit'
+
+import { useState, useRef, useCallback } from "react";
+import upload from './upload-icon.png' ;
+
 
 function App() {
 
   const inpRef = useRef(null);
-
   const [file, setFile] = useState("");
-  // const [src, setSrc] = useState("")
+  const [crop, setCrop] = useState({ x: 0, y: 0 });
 
   function handleChange(e) {
     console.log(e.target.files);
     setFile(URL.createObjectURL(e.target.files[0]));
   }
 
-  function uploadImage(){
-    inpRef.current.click();
-  }
-
   return (
     <div className="App">
-      <h2>Insert your image here</h2>
+      <h2>Tap on the upload icon to insert your image</h2>
       <div>
-        {/* <img width={100} src='../public/upload-icon.png' onClick={uploadImage}/> */}
-        <input type="file" onChange={handleChange} ref={inpRef}/><br/><br/><br/>
+        <img width={100} src={upload} onClick={() => {inpRef.current.click();}}/>
+        <input type="file" onChange={handleChange} ref={inpRef} style={{display: "none"}}/><br/><br/><br/>
       </div>
       <button className="clear-image" onClick={() => {setFile()}}>Clear</button><br/><br/><br/>
       <button className="crop">Crop</button><br/><br/><br/>
-      {file ? (
-        <img src={file} alt=""/>
+      {/* {file ? (
+        <img src={file} alt="ff"/>
         ) : (
-          <img src="./upload-icon.png" alt="rr"/>
-      )}
+          <img src={upload} width={40} alt="rr"/>
+          )} */}
+        <img src={file} alt="no image selected"/>
     </div>
   );
 }
