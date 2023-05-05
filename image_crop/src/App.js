520cedd830554595a97997748b8f99b3c7e80499
Shoydon <shoydon@gmail.com>
2023-05-05 22:00:48 +0530

    commit

--- image_crop/src/App.js
+++ image_crop/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-// import ReactCrop from 'react-image-crop'
+import ReactCrop from 'react-image-crop'
 // import 'react-image-crop/dist/ReactCrop.css'
 // import Cropper from 'react-easy-crop'
-import Avatar from 'react-avatar-edit'
+import Cropper from 'cropperjs'
 
-import { useState, useRef, useCallback } from "react";
+import { useState, useRef, useEffect } from "react";
 import upload from './upload-icon.png' ;
 
 
@@ -12,11 +12,38 @@ function App() {
 
   const inpRef = useRef(null);
   const [file, setFile] = useState("");
-  const [crop, setCrop] = useState({ x: 0, y: 0 });
+  const [crop, setCrop] = useState();
+  const [cropWidth, setCropWidth] = useState(200);
+  const [cropHeight, setCropHeight] = useState(200);
 
+  
   function handleChange(e) {
     console.log(e.target.files);
     setFile(URL.createObjectURL(e.target.files[0]));
+
+    const image = document.getElementById("img")
+    const cropper = new Cropper(image, {
+      aspectRatio: 16 / 9,
+      crop(ev){
+        console.log(ev.detail.x);
+        console.log(ev.detail.y);
+        console.log(ev.detail.width);
+        console.log(ev.detail.height);
+        console.log(ev.detail.rotate);
+        console.log(ev.detail.scaleX);
+        console.log(ev.detail.scaleY);
+      },
+      autoCrop: false,
+      
+    })
+  }
+
+  function editImage(){
+    return(
+      <div>
+
+      </div>
+    )
   }
 
   return (
@@ -33,7 +60,8 @@ function App() {
         ) : (
           <img src={upload} width={40} alt="rr"/>
           )} */}
-        <img src={file} alt="no image selected"/>
+        <img src={file} alt="no image selected" id="img"/>
+        <div className='img-prev'></div>
     </div>
   );
 }
